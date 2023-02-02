983b6663e867a788489fff2b52191d45379ff80b
GerardoITA <gerardo.giusti@tiscali.it>
2023-02-02 22:36:35 +0100

    esercizio completato

---
+++ database/migrations/2023_02_02_190026_create_saints_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('saints', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 64);
+            $table->string('birthplace', 64);
+            $table->date('canonization');
+            $table->integer('miracles')->unsigned();
+            $table->timestamps();
+           
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('saints');
+    }
+};
